b1c1aab4e73f1008ed4e1fbbd73d153ec94cbd3f
David O'Brien <ddrobrien@icloud.com>
2022-08-28 13:49:58 +0000

    add input for number generator and welcome player message

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+number = random.randint(1, 20)
+
+player_name = input("Hello, What's your name?")
