09df0a8bdd951ff84da83bc8a5ecb9409645913f
David O'Brien <ddrobrien@icloud.com>
2022-08-28 14:00:34 +0000

    guessing loop created with statements for user guessing too high or too low

--- run.py
+++ run.py
@@ -5,3 +5,13 @@ number = random.randint(1, 20)
 player_name = input("Hello, What's your name?")
 
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 20:')
+
+while number_of_guesses < 5:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('Your guess is too low')
+    if guess > number:
+        print('Your guess is too high')
+    if guess == number:
+        break
