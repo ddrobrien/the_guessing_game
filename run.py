60a5f472dbe009a8b0bbe199835769b85dc76401
David O'Brien <ddrobrien@icloud.com>
2022-09-01 12:45:37 +0000

    includes a loop at the end of the code, to ask users if they want to play again

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import random
 
+number = random.randint(1, 20)
+
+
 def validate_number_input(data):
     """ This method validates that the input is a number from 1 to 20 """
     if data.isdigit():
@@ -13,31 +16,52 @@ def validate_number_input(data):
         print('What you have entered is not a number!')
         return False
     return True
-    
-number = random.randint(1, 20)
-player_name = input("Hello, please your name, if you enter nothing we will just call you player one:").strip().lower()
-#validation....
-if player_name == "":
-    player_name = "player one"
-    
-number_of_guesses = 0
-print(f'okay! {player_name} I am guessing a number bewween 1 and 20, what am i guessing?')
+
+
+def get_player_name():
+    player_name = input("Hello, please enter your name, if nothing is entered we will call you player one:").strip().lower()
+    #validation....
+    if player_name == "":
+        player_name = "player one"
+    return player_name
+
+
+def game_loop(player_name):
+    number_of_guesses = 0
+    print(f'okay! {player_name} I am guessing a number bewween 1 and 20, what am i guessing?')
+
 while number_of_guesses < 5:
     player_guess = input().strip()
     # check that what is entered is a number
     check_guess = validate_number_input(player_guess)
-    
+
     if check_guess:
         guess = int(player_guess)
-    
-        number_of_guesses += 1
-        if guess < number:
-            print('Your guess is too low')
-        if guess > number:
-            print('Your guess is too high')
-        if guess == number:
-            break
-        if guess == number:
-            print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
+
+    number_of_guesses += 1
+if guess < number:
+    print('Your guess is too low')
+if guess > number:
+    print('Your guess is too high')
+if guess == number:
+    print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
+    break
+
+if number_of_guesses >= 5:
+        print('You did not guess the number, The number was ' + str(number))
+
+
+def main():
+    player_name = get_player_namer()
+    while True:
+        game_loop(player_name)
+        ask_at_end = input('Would you like another game? Hit any key to go again or type n or no to stop').strip().lower()
+        answer_no = ['n', 'no']
+        if ask_at_end in answer_no:
+            print(f"Thank's for playing {player_name}, hope to see you again soon")
+            exit()
         else:
-            print('You did not guess the number, The number was ' + str(number))
+            continue
+
+if __name__ == "__main__":
+    main()
